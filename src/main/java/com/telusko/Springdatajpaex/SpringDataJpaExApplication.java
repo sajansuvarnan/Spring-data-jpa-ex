1c69da9ef0c37e584e96d3ddca7842e1c165fd56
sajansuvarnan <sajan.suvarnan@gmail.com>
2025-02-03 23:24:50 +0530

    feat: added logic for update and delete

--- src/main/java/com/telusko/Springdatajpaex/SpringDataJpaExApplication.java
+++ src/main/java/com/telusko/Springdatajpaex/SpringDataJpaExApplication.java
@@ -45,15 +45,36 @@ public class SpringDataJpaExApplication {
 		repo.save(s3);
 		repo.save(s4);*/
 
+		//SELECT QUERY
 
 		//To find all data
-		System.out.println(repo.findAll());
+		//System.out.println(repo.findAll());
 
 
 		//when the data is not found, it will throw a exception, so we are storing the data
 		//And if the value is not present create a dummy object
-		Optional<Student> s = repo.findById(1);
-		System.out.println(s.orElse(new Student()));
+		//Optional<Student> s = repo.findById(1);
+		//System.out.println(s.orElse(new Student()));
+
+		//Inbuild query
+		//System.out.println(repo.findByName("Aswin"));
+		//System.out.println(repo.findByMarksGreaterThan(80));
+
+
+		//own query
+		//System.out.println(repo.findByNameWithAlpha("A"));
+
+		//UPDATE QUERY
+
+		//s4.setRollNo(4);
+		//s4.setName("Kasi Vishwa");
+		//s4.setMarks(90);
+
+		//repo.save(s4);
+
+		//DELETE QUERY
+
+		// repo.delete(s4);
 
 	}
 
--- src/main/java/com/telusko/Springdatajpaex/StudentRepo.java
+++ src/main/java/com/telusko/Springdatajpaex/StudentRepo.java
@@ -2,8 +2,18 @@ package com.telusko.Springdatajpaex;
 
 import com.telusko.Springdatajpaex.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StudentRepo extends JpaRepository<Student,Integer> {
+
+    List<Student> findByName(String aswin);
+
+    List<Student> findByMarksGreaterThan(int i);
+
+    @Query("Select s from Student s where s.name like ?1%")
+    List<Student> findByNameWithAlpha(String a);
 }
