e827c1c31c48059bffe9c67133e58c6ebe1e8e32
sajansuvarnan <sajan.suvarnan@gmail.com>
2025-02-03 16:54:37 +0530

    New code

---
+++ src/main/java/com/telusko/Springdatajpaex/SpringDataJpaExApplication.java
@@ -0,0 +1,43 @@
+package com.telusko.Springdatajpaex;
+
+import com.telusko.Springdatajpaex.model.Student;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringDataJpaExApplication {
+
+	public static void main(String[] args) {
+
+
+		ApplicationContext context = SpringApplication.run(SpringDataJpaExApplication.class, args);
+
+		StudentRepo repo = context.getBean(StudentRepo.class);
+
+		Student s1 = context.getBean(Student.class);
+		Student s2 = context.getBean(Student.class);
+		Student s3 = context.getBean(Student.class);
+		Student s4 = context.getBean(Student.class);
+
+		s1.setRollNo(1);
+		s1.setName("Aswin");
+		s1.setMarks(87);
+
+
+		s2.setRollNo(2);
+		s2.setName("Arun Kumar MG");
+		s2.setMarks(79);
+
+
+		s3.setRollNo(3);
+		s3.setName("Mohan Raj");
+		s3.setMarks(86);
+
+		s4.setRollNo(4);
+		s4.setName("Kasi Vishwa");
+		s4.setMarks(90);
+
+	}
+
+}
---
+++ src/test/java/com/telusko/Springdatajpaex/SpringDataJpaExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telusko.Springdatajpaex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
